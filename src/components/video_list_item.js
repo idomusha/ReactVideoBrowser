31069044b5cf24b9a9a10f9e60e8bcb1b2b62c08
idomusha <idomusha@muskrat.tk>
2017-05-14 16:33:48 +0200

    final commit

---
+++ src/components/video_list_item.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const VideoListItem = ({video, onVideoSelect}) => { // const video = props.video;
+  // const video = props.video;
+  // const onVideoSelect = props.onVideoSelect;
+  const imageUrl = video.snippet.thumbnails.default.url;
+  const title = video.snippet.title;
+
+  return (
+    <li className="list-group-item" onClick={() => onVideoSelect(video)}>
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import React, { Component} from 'react';
+import ReactDOM from 'react-dom';
+import  YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideosList from './components/video_list';
+import VideosDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyAvN9aFp4kzn1W6oz7IgPiiauxHOgaxxzE';
+
+// create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('robot');
+
+  }
+
+  videoSearch(term) {
+    // fetch data
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideosDetail video={this.state.selectedVideo} />
+        <VideosList
+          onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+
+// Take this component's generated HTML and put it on the page
+ReactDOM.render(<App />, document.querySelector('.container'));
